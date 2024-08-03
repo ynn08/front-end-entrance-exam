1fe9b7110d75de3913831edaf880b76dbf53018f
yn <anderson.yndm@gmail.com>
2024-08-03 16:24:56 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 const button =
-document.querySelector('.download-btn');
+    document.querySelector('.download-btn');
 button.addEventListener('click',
-() => {
-    alert("clicked");
-});
+    () => {
+        alert("clicked");
+    });
 
-const input = document.querySelectorAll('.interests-box__tag'); 
+const input = document.querySelectorAll('.interests-box__tag');
 const interestsBoxContent = document.querySelector('.interests-box__content');
-for (let i = 0; i < 10; ++i){ 
-    input[i].addEventListener('input', resizeInput); 
+for (let i = 0; i < 10; ++i) {
+    input[i].addEventListener('input', resizeInput);
     resizeInput.call(input[i]);
 
 }
@@ -22,12 +22,24 @@ function resizeInput() {
 
 const page = document.querySelector('.wrapper');
 
+// const opt = {
+//     margin: 0.3,
+//     filename: "resume.pdf",
+//     image: { type: "svg", quality: 0.98 },
+//     html2canvas: { scale: 4 },
+//     jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
+//   };
+
+// html2pdf().from(page).set(opt).save();
+// alert("dsjflsdf");
+
+
 const opt = {
     margin: 0.3,
     filename: "resume.pdf",
-    image: { type: "svg", quality: 0.98 },
+    image: { type: "jpeg", quality: 0.98 },
     html2canvas: { scale: 4 },
     jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
-  };
+};
 
 html2pdf().from(page).set(opt).save();
