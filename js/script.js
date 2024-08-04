9b63dd524b6f4a3f938de207e95e1fe09cd03507
yn <anderson.yndm@gmail.com>
2024-08-04 10:03:34 +0300

    adding working version

--- js/script.js
+++ js/script.js
@@ -1,12 +1,4 @@
-const button =
-    document.querySelector('.download-btn');
-button.addEventListener('click',
-    () => {
-        alert("clicked");
-    });
-
 const input = document.querySelectorAll('.interests-box__tag');
-const interestsBoxContent = document.querySelector('.interests-box__content');
 for (let i = 0; i < 10; ++i) {
     input[i].addEventListener('input', resizeInput);
     resizeInput.call(input[i]);
@@ -19,27 +11,63 @@ function resizeInput() {
     }
 }
 
+function saveChanges() {
+    const inputFields = document.querySelectorAll("input");
+    const areaFields = Array.from(document.querySelectorAll("textarea"));
 
-const page = document.querySelector('.wrapper');
+    const inputData = [];
+    for (let i = 0; i < inputFields.length; ++i) {
+        inputData[i] = inputFields[i].value;
+    }
 
-// const opt = {
-//     margin: 0.3,
-//     filename: "resume.pdf",
-//     image: { type: "svg", quality: 0.98 },
-//     html2canvas: { scale: 4 },
-//     jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
-//   };
+    const areaData = [];
+    for (let i = 0; i < areaFields.length; ++i) {
+        areaData[i] = areaFields[i].value;
+    }
 
-// html2pdf().from(page).set(opt).save();
-// alert("dsjflsdf");
+    localStorage.setItem("inputData", JSON.stringify(inputData));
+    localStorage.setItem("areaData", JSON.stringify(areaData));
+}
 
+const button =
+    document.querySelector('.download-btn');
+button.addEventListener('click',
+    () => {
 
-const opt = {
-    margin: 0.3,
-    filename: "resume.pdf",
-    image: { type: "jpeg", quality: 0.98 },
-    html2canvas: { scale: 4 },
-    jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
-};
+        saveChanges();
 
-html2pdf().from(page).set(opt).save();
+        const content = document.querySelector(".resume-page");
+
+        const opt = {
+            margin: 0.2,
+            filename: 'resume.pdf',
+            image: { type: 'jpeg', quality: 0.98 },
+            html2canvas: { scale: 2 },
+            jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+        };
+
+        html2pdf().from(content).set(opt).save();
+    });
+
+function getChanges() {
+    const inputFields = document.querySelectorAll("input");
+    const areaFields = document.querySelectorAll("textarea");
+
+    const inputData = JSON.parse(localStorage.getItem("inputData"));
+    const areaData = JSON.parse(localStorage.getItem("areaData"));
+
+    if (!inputData || !areaData) {
+        return;
+    }
+
+    for (let i = 0; i < inputFields.length; ++i) {
+        inputFields[i].value = inputData[i];
+    }
+
+    for (let i = 0; i < areaFields.length; ++i) {
+        areaFields[i].value = areaData[i];
+    }
+}
+
+window.onclick = saveChanges;
+window.onload = getChanges;
